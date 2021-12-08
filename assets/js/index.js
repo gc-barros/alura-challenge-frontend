9822a68c16acafb8a0a74205e8f8d81d311f1499
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-12-07 21:22:07 -0300

    Ajustes gerais

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 import initCabecalho from "./cabecalho.js"
 
 initCabecalho();
+
+
+// Testes provisórios:
+const highlightButton = document.querySelector('[data-highligth-botao]');
+highlightButton.addEventListener('click', function () {
+    this.classList.toggle("editor__botao--highlight-ativo");
+})
+
+const saveButton = document.querySelector("[data-salvar-botao]");
+saveButton.addEventListener('click', (e) => {
+    e.preventDefault();
+})
